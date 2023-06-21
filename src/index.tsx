f016445896b2f994f03d22a33c6403fc92f346e4
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-21 19:45:48 +0300

    try to build

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+
+//import { App } from './App';
+
+const rootElement = document.getElementById('root');
+
+render(<div>
+    AAAAAAAAAAvfgdgdfg
+</div>, rootElement);
