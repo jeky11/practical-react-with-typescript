652528fda05147ac321bbb622d98fc19e53c8cc4
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-23 17:21:28 +0300

    counter

---
+++ src/components/Scoreboard/Counter.test.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Counter } from './Counter';
+
+it('Counter renders correctly', () => {
+  const { asFragment } = render(<Counter>001</Counter>);
+
+  expect(asFragment()).toMatchSnapshot();
+});
