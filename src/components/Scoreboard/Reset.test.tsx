ac8246720b845d69e0092da9879dcf588f199f2a
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-23 18:35:45 +0300

    reset

---
+++ src/components/Scoreboard/Reset.test.tsx
@@ -0,0 +1,46 @@
+﻿import React, { FC } from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import { Reset } from './Reset';
+
+describe('Reset button check', () => {
+  const ResetWithDummyHandlerOnReset: FC = () => <Reset onReset={() => null} />;
+  it('Reset renders correctly', () => {
+    const onReset = jest.fn();
+
+    const { asFragment } = render(<Reset onReset={onReset} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('Should render elements with default state', () => {
+    render(<ResetWithDummyHandlerOnReset />);
+
+    expect(screen.getByText('🙂')).toBeInTheDocument();
+  });
+  it('onReset handler should be called', () => {
+    const onReset = jest.fn();
+
+    render(<Reset onReset={onReset} />);
+    fireEvent.click(screen.getByText('🙂'));
+
+    expect(onReset).toBeCalled();
+  });
+  it('Should change state when onMouseDown and onMouseUp events happened', () => {
+    render(<ResetWithDummyHandlerOnReset />);
+
+    fireEvent.mouseDown(screen.getByText('🙂'));
+    expect(screen.getByText('😯')).toBeInTheDocument();
+
+    fireEvent.mouseUp(screen.getByText('😯'));
+    expect(screen.getByText('🙂')).toBeInTheDocument();
+  });
+  it('Should change state when onMouseDown, and onMouseLeave events happened', () => {
+    render(<ResetWithDummyHandlerOnReset />);
+
+    fireEvent.mouseDown(screen.getByText('🙂'));
+    expect(screen.getByText('😯')).toBeInTheDocument();
+
+    fireEvent.mouseLeave(screen.getByText('😯'));
+    expect(screen.getByText('🙂')).toBeInTheDocument();
+  });
+});
---
+++ src/components/Scoreboard/Reset.tsx
@@ -0,0 +1,40 @@
+﻿import React, { memo } from 'react';
+import styled from '@emotion/styled';
+
+import { useMouseDown } from '@/components/hooks/useMouseDown';
+
+export interface ResetProps {
+  /**
+   * Reset action handler
+   */
+  onReset: () => void;
+}
+
+export const Reset = memo(({ onReset }: ResetProps): JSX.Element => {
+  const [mouseDown, onMouseDown, onMouseUp] = useMouseDown();
+
+  return (
+    <Button
+      onMouseDown={onMouseDown}
+      onMouseLeave={onMouseUp}
+      onMouseUp={onMouseUp}
+      onClick={onReset}
+    >
+      {mouseDown ? '😯' : '🙂'}
+    </Button>
+  );
+});
+
+// Stryker disable next-line StringLiteral
+Reset.displayName = 'Reset';
+
+const Button = styled.button`
+  font-size: 1.1vw;
+  height: 100%;
+  cursor: pointer;
+  font-weight: 700;
+  border-width: 0.15vw;
+  border-style: solid;
+  background-color: #d1d1d1;
+  border-color: white #9e9e9e #9e9e9e white;
+`;
