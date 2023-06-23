7bb128175e54b0c7a77b1f858e3540341222f044
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-23 18:52:12 +0300

    scoreboard

---
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -0,0 +1,59 @@
+﻿import React, { ChangeEvent } from 'react';
+import styled from '@emotion/styled';
+
+import { Counter } from './Counter';
+import { Level } from './Level';
+import { Reset } from './Reset';
+
+export interface ScoreboardProps {
+  /**
+   * Timer
+   */
+  time: string;
+  /**
+   * Possible game scenarios
+   */
+  levels: string[];
+  /**
+   * Default selected level
+   */
+  defaultLevel?: string;
+  /**
+   * Action handler when the GameReset button is clicked
+   */
+  onReset: () => void;
+  /**
+   * Action handler when select new lvl
+   */
+  onChangeLevel: (event: ChangeEvent<HTMLSelectElement>) => void;
+  /**
+   * Bombs in the field
+   */
+  bombs: string;
+}
+
+export const Scoreboard = ({
+  time,
+  levels,
+  defaultLevel,
+  bombs,
+  onReset,
+  onChangeLevel: onChange,
+}: ScoreboardProps): JSX.Element => (
+  <Wrapper>
+    <Counter>{time}</Counter>
+    <div>
+      <Level onChange={onChange} value={defaultLevel}>
+        {levels}
+      </Level>
+      <Reset onReset={onReset} />
+    </div>
+    <Counter>{bombs}</Counter>
+  </Wrapper>
+);
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,22 @@
 import ReactDOM from 'react-dom';
 
 import { Top } from '@/components/top';
+import { Scoreboard } from '@/components/Scoreboard/Scoreboard';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <Top feature="flag" firstAction="ctrl" secondAction="click">
-    Minesweeper
-  </Top>,
+  <>
+    <Top feature="flag" firstAction="ctrl" secondAction="click">
+      Minesweeper
+    </Top>
+    <Scoreboard
+      time="000"
+      levels={['1', '2', '3']}
+      onReset={() => null}
+      onChangeLevel={() => null}
+      bombs="010"
+    />
+  </>,
   rootElement
 );
