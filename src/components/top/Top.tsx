6b8c60bf4d2d7cdcf5d0868f3af429c66a3e1830
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-23 16:52:39 +0300

    top components

---
+++ src/components/top/Top.tsx
@@ -0,0 +1,25 @@
+﻿import React, { memo } from 'react';
+import styled from '@emotion/styled';
+
+import { Legend, LegendProps } from './Legend';
+import { GameName, GameNameProps } from './GameName';
+
+export type TopComponentType = LegendProps & GameNameProps;
+
+export const Top = memo(
+  ({ children, ...legendProps }: TopComponentType): JSX.Element => (
+    <Header>
+      <GameName>{children}</GameName>
+      <Legend {...legendProps} />
+    </Header>
+  )
+);
+
+// Stryker disable next-line StringLiteral
+Top.displayName = 'Top';
+
+const Header = styled.header`
+  text-align: center;
+  position: relative;
+  display: inline-block;
+`;
